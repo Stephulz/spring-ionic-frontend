c812fc70d6b66c3b44b4633588189b1ad2c7237a
Stefan Sanches <stefansanches9@gmail.com>
2019-04-18 10:36:30 -0300

    Showing categories

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -8,6 +10,9 @@ import { CategoriaService } from '../../services/domain/categoria.service';
   templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
+
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+  items: CategoriaDTO[];
 
   constructor(
     public navCtrl: NavController,
@@ -17,12 +22,14 @@ export class CategoriasPage {
   }
 
   ionViewDidLoad() {
-    this.categoriaService.findAll().subscribe(res => {
-      console.log(res);
-    },
-      error => {
-      console.log(error);
-      });
+    this.categoriaService.findAll()
+      .subscribe(res => {
+        console.log("this.categoriaService.findAll() ", res);
+        this.items = res;
+      },
+        error => {
+          console.log("ERROR: ", error);
+        });
     console.log('ionViewDidLoad CategoriasPage');
   }
 
