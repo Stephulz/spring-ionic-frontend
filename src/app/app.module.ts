55750020cc1982f8b52d4b9a2400aeee9b9ff8a3
Stefan Sanches <stefansanches9@gmail.com>
2019-04-23 08:56:27 -0300

    Interceptor to include JWT token on requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
 import { ClienteService } from '../services/domain/cliente.service';
+import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { ClienteService } from '../services/domain/cliente.service';
     StatusBar,
     SplashScreen,
     CategoriaService,
+    AuthInterceptorProvider,
     ErrorInterceptorProvider,
     AuthService,
     StorageService,
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -11,19 +11,14 @@ export class ClienteService {
     constructor(public http: HttpClient, public storage: StorageService) {
 
     }
-    
-    findByEmail(email: string) : Observable<ClienteDTO> {
 
-        let token = this.storage.getLocalUser().token;
-        let authHeader = new HttpHeaders({'Authorization': "Bearer " + token});
-        
+    findByEmail(email: string): Observable<ClienteDTO> {
         return this.http.get<ClienteDTO>(
-            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`,
-            {'headers': authHeader});
+            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
-    getImageFromBucket(id: string) : Observable<any> {
+    getImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
-        return this.http.get(url, {responseType : 'blob'});
+        return this.http.get(url, { responseType: 'blob' });
     }
 }
