88c3f5eacf4042af45b7e0d80230e1b88727e8c7
Stefan Sanches <stefansanches9@gmail.com>
2019-05-06 09:32:52 -0300

    Access to cart: Floating button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,7 @@ export class MyApp {
     this.pages = [
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriasPage' },
+      { title: 'Carrinho', component: 'CartPage'},
       { title: 'Logout', component: '' },
     ];
 
