29f03f740ae69be660ad3d7e3b61bb791401254e
Stefan Sanches <stefansanches9@gmail.com>
2019-05-16 09:32:58 -0300

    Loading component

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
@@ -16,34 +16,48 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProdutosPage');
 
     let categoria_id = this.navParams.get('categoria_id');
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(res => {
         this.items = res['content'];
         this.loadImageUrls();
+        loader.dismiss();
       },
-        error => { });
+        error => {
+          loader.dismiss();
+        });
   }
 
-  loadImageUrls() {
+  loadImageUrls() {    
     for (var i = 0; i < this.items.length; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(res => {
           item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`
         },
-          error => { });
-    }
+          error => {            
+          });
+    }    
   }
 
   showDetail(produtoId: string) {
-    this.navCtrl.push('ProdutoDetailPage', {produto_id: produtoId});
+    this.navCtrl.push('ProdutoDetailPage', { produto_id: produtoId });
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Carregando..."
+    });
+    loader.present();
+    return loader;
   }
 
 }
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams} from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
@@ -18,7 +18,8 @@ export class ProfilePage {
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+  ) {
   }
 
   ionViewDidLoad() {
